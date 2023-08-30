50b9f64999dbe62bb810d10c9bd2f4d89bbfb08e
LeandroBorloni <lele6133@gmail.com>
2023-08-29 22:38:41 -0300

    Cards

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,19 @@
+import CardFilme from "@/components/CardFilme";
 import Title from "@/components/Title";
 
 export default function Home() {
+  const filmes = [
+    {
+      titulo: "Megatubarão 2",
+      nota: 6.2,
+      poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8tBhAn6qVRQzf5yvEcxjgPMgTkw.jpg"
+    },
+    {
+      titulo: "Barbie",
+      nota: 7.4,
+      poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/yRRuLt7sMBEQkHsd1S3KaaofZn7.jpg"
+    }
+  ]
   return (
     <>
       <nav className="flex p-4 bg-slate-900">
@@ -22,17 +35,13 @@ export default function Home() {
           </li>
         </ul>
       </nav>
-      <Title>Favoritos</Title>
-      <div id="card" className="flex flex-col w-40 justify-center items-center m-2">
-        <img className="rounded" src="https://place-hold.it/150x220/666" alt=""></img>
-        <span className="font-bold text-center line-clamp-1">titulo do filme do card</span>
-        <div>
-          <span>6.0</span>
-        </div>
-        <a href="#" className="bg-pink-600 py-2 w-full rounded text-center">Detalhes</a>
-      </div>
+      <Title>Em alta</Title>
+      <section className="flex flex-wrap">
+        {filmes.map(filme => <CardFilme filme={filme}></CardFilme>)}
 
-      <Title>Filmes</Title>
+      </section>
+      <Title>Lançamentos</Title>
+      <Title>Favoritos</Title>
 
     </>
   )
